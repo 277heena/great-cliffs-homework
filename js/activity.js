0f589e0049df2e1072ff195c16ef6b3442ed9b07
Heena Patel <277heena@gmail.com>
2026-03-30 20:22:40 -0700

    Fix activities table script links

--- js/activity.js
+++ js/activity.js
@@ -1,12 +1,8 @@
-activity.js
 $(document).ready(function () {
     $(".activities-table td").each(function () {
         var cellText = $(this).text().trim();
 
-        if (
-            cellText !== "Not Available" &&
-            $(this).index() !== 0
-        ) {
+        if (cellText !== "Not Available" && $(this).index() !== 0) {
             $(this).addClass("selectable");
         } else {
             $(this).addClass("not-selectable");
