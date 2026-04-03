b41fc2de6d6cf004ca9f5a8b121b58a5fe7fc5c4
277heena <277heena@gmail.com>
2026-04-02 20:09:56 -0700

    Finish activities homework with jQuery selection box

--- js/activity.js
+++ js/activity.js
@@ -10,6 +10,29 @@ $(document).ready(function () {
     });
 
     $(".activities-table td.selectable").click(function () {
+        var content = $(this).text().trim();
+        var columnIndex = $(this).index();
+        var cliffName = $(".activities-table tr").eq(1).find("th").eq(columnIndex).text().trim();
+        var itemKey = content.replace(/\s+/g, "-").toLowerCase() + "-" + cliffName.replace(/\s+/g, "-").toLowerCase();
+
         $(this).toggleClass("selected");
+
+        if ($(this).hasClass("selected")) {
+            $("#displaySelected").css("visibility", "visible");
+            $("#displaySelected").css("margin-top", "2em");
+
+            $("#result").append(
+                '<p data-key="' + itemKey + '">' +
+                content + ' <span class="cliff-name">at ' + cliffName + '</span>' +
+                '</p>'
+            );
+        } else {
+            $('#result p[data-key="' + itemKey + '"]').remove();
+
+            if ($("#result p").length === 0) {
+                $("#displaySelected").css("visibility", "hidden");
+                $("#displaySelected").css("margin-top", "0");
+            }
+        }
     });
 });
